618b06f90d70b514aed103962134b936ff644359
spencerthomas <tom@cognitives.io>
2018-04-17 14:14:49 +1000

    update migrations

--- db/migrate/20170718110810_add_stripe_to_users.rb
+++
@@ -1,6 +0,0 @@
-riclass AddStripeToUsers < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :users, :subscribed, :string, default: "basic"
-  	add_column :users, :stripeid, :string
-  end
-end
